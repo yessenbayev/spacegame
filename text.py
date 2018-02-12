b83693af1c81ace490bc50a5a13588efe5cfe668
Alan' Yessenbayev <alan.yessenbayev@gmail.com>
2018-02-11 23:30:57 -0800

    I am an idiot

---
+++ text.py
@@ -0,0 +1,33 @@
+from init import *
+import pygame
+    
+
+def text_objects(text,font,size,color):
+    '''Parses text to gameDisplay.blit(TextSurf,TextRect)'''
+    font = pygame.font.Font(font,size)
+    TextSurf = font.render(text, True, color)
+    TextRect = TextSurf.get_rect()
+    return TextSurf, TextRect
+
+GameOverSurf, GameOverRect = text_objects('Game Over',
+                                          'freesansbold.ttf',
+                                          115,(0,0,0))
+GameOverRect.center = (display_width/2,display_height/2)
+
+GameOverSurf2, GameOverRect2 = text_objects('Press ENTER to start the game again',
+                                          'freesansbold.ttf',
+                                          50,(0,0,0))
+GameOverRect2.center = (display_width/2,display_height/2+100)
+
+def print_score(score,GameOver):
+    ts, tr = text_objects('Score: {}'.format(score),'freesansbold.ttf',
+                          50, (0,0,0))
+    tr.topright = (display_width,0)
+    gameDisplay.blit(ts,tr)
+    
+    if GameOver:
+        GameOverSurf, GameOverRect = text_objects('Score:{}'.format(score),
+                                          'freesansbold.ttf',
+                                          50,(0,0,0))
+        GameOverRect.center = (display_width/2,display_height/2-100)
+        gameDisplay.blit(GameOverSurf,GameOverRect)
