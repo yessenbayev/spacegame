f1707c9a053700f0f2e784f10ecb3f6c373dfb29
yessenbayev <alan.yessenbayev@gmail.com>
2022-09-09 20:50:15 -0700

    Add __future__

--- space.py
+++ space.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 from init import *
 from obj import *
 import text
--- text.py
+++ text.py
@@ -1,6 +1,6 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 from init import *
 import pygame
-
 
 def text_objects(text,font,size,color):
     '''Parses text to gameDisplay.blit(TextSurf,TextRect)'''
